1517a660cfaca4ef522f31fda15d862718670a7b
Hexxxxxxx <67011212055@msu.ac.th>
2025-07-17 18:02:21 +0700

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,29 @@
 package src;
+import java.io.*;
+import java.net.*;
 
 public class Server {
-}
+    public static void main(String[] args) throws IOException {
+        try{
+           ServerSocket serverSocket = new ServerSocket(8080);
+           System.out.println("Server started");
+           while(true){
+               Socket socket = serverSocket.accept();
+               System.out.println("Client connected");
+               BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+               PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+               String inputLine;
+               while((inputLine = in.readLine()) != null){
+                   System.out.println(inputLine);
+                   out.println(inputLine);
+               }
+               in.close();
+               out.close();
+               socket.close();
+           }
+        }
+        catch(IOException ioe){
+            System.err.println(ioe);
+        }
+    }
+} 
