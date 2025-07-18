c421bbe1eed3515e1fdffeabaff73219aa4bb60c
Tafire13 <67011212063@msu.ac.th>
2025-07-18 17:58:14 +0700

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,7 @@
 package src;
 import java.io.*;
 import java.net.*;
-
+import java.util.*;
 public class Server {
     public static void main(String[] args) throws IOException {
         try{
@@ -10,9 +10,19 @@ public class Server {
            while(true){
                Socket socket = serverSocket.accept();
                System.out.println("Client connected");
-               BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-               PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-               out.println("Hello from server"+new java.util.Date());
+               BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));// from client
+               PrintWriter out = new PrintWriter(socket.getOutputStream(), true);// to server
+               String name = in.readLine();
+               String sid = in.readLine();
+               String ServerName = InetAddress.getLocalHost().getHostName();
+               String date = new Date().toString();
+               System.out.println("Received name from client is "+name);
+               System.out.println("Received student id from client is "+sid);
+               out.println(name);
+               out.println(sid);
+               out.println(ServerName);
+               out.println(date);
+               System.out.println("Sending to client");
                socket.close();
            }
         }
